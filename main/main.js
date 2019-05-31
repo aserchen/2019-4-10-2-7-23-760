78732bb6dd69a8f6da295d2b1eff2acccb233b2a
aserchen <2446922621@qq.com>
2019-05-31 20:30:04 +0800

    coding

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var price=0;
+	if(inputs['distance']>=8){
+		price = 6+ 4.8 +1.2*(inputs['distance']-8);
+	}else if(inputs['distance']>=2){
+		price = 6+ 0.8*(inputs['distance']-2);
+	}else{
+		price = 6;
+	}
+	
+	price += inputs['parkTime']*0.25;
+    return Math.round(price);
 };
